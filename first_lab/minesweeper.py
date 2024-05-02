b822dfc9280f9138029f2cc3a6084a78ffe40970
Maria Marchenko <maria.dmit.rovna.slv@gmail.com>
2024-05-02 19:32:14 +0300

    Add label refreshing utility

--- first_lab/minesweeper.py
+++ first_lab/minesweeper.py
@@ -29,3 +29,49 @@ class Minesweeper:
 
         self.setup(num_mines)
         self.updateTimer()
+
+    def setup(self, num_mines):
+        self.flagCount = 0
+        self.clickedCount = 0
+        self.startTime = None
+        self.tiles = {}
+        self.mines = num_mines 
+        all_positions = [(x, y) for x in range(self.size_x) for y in range(self.size_y)]
+        mines_positions = set(random.sample(all_positions, self.mines))
+
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                isMine = (x, y) in mines_positions
+                id = f"{x}_{y}"
+                tile = {
+                    "id": id,
+                    "isMine": isMine,
+                    "state": 0,
+                    "coords": {"x": x, "y": y},
+                    "button": Button(self.frame, text=" ", width=4, height=2),
+                    "mines": 0
+                }
+                tile["button"].grid(row=x+1, column=y)
+                tile["button"].bind("<Button-1>", self.onClickWrapper(x, y))
+                tile["button"].bind("<Button-3>", self.onRightClickWrapper(x, y))
+                if x not in self.tiles:
+                    self.tiles[x] = {}
+                self.tiles[x][y] = tile
+
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                tile = self.tiles[x][y]
+                mine_count = sum((1 for n in self.getNeighbors(x, y) if n["isMine"]))
+                tile["mines"] = mine_count
+
+        self.refreshLabels()
+
+    def refreshLabels(self):
+        self.labels["flags"].config(text=f"Flags: {self.flagCount}")
+        self.labels["mines"].config(text=f"Mines: {self.mines}")
+
+    def onClickWrapper(self, x, y):
+        return lambda Button: self.onClick(self.tiles[x][y])
+
+    def onRightClickWrapper(self, x, y):
+        return lambda Button: self.onRightClick(self.tiles[x][y])
